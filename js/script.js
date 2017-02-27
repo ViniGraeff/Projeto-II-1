978adfcd07acc300da6995063a6630a84e7d08c8
Millena Martins <Millena Martins>
2017-02-27 08:38:28 -0300

    atualizaçõ ajustes

--- js/script.js
+++ js/script.js
@@ -1,48 +1,19 @@
-var server = "http://192.168.1.172:3000/product/";//servidos com os dados
-var checkName = 0;
+var server = "http://localhost:3000/product/";//servidor com os dados
+var checkName = 0;//variavel para checar de o item ja existe
 
-function cleanTable(){//limpa a tabela
+//limpa a tabela
+function cleanTable(){
 	$("#rowsTable").html("");
 }
 
-function cleanBusca(){
-	$("#listaBusca").html("");
-	$("#campoBusca").val("");
-	$(".listaBusca").fadeOut("fast").css({"display":"none"});
+//limpa o campo de busca e os resultados
+function searchClean(){
+	$("#searchResult").html("");
+	$("#searchInput").val("");
+	$(".searchResult").fadeOut("fast").css({"display":"none"});
 }
 
-function createTable(){//cria a tabela através do get
-	var statusSelected = checkStatusSelect($("#selectStatus").val());
-	cleanTable();//limpa a tabela para atualizar
-	cleanBusca();
-	$.get(server, function(data) {//seleciona os dados no json
-		for(var i=0; i<data.length; i++){//percorre todos os dados
-			var valor = valueToString(data[i].valor);
-			var status = verifyStatus(data[i].status);
-			if(statusSelected==data[i].status){
-				$("#rowsTable").append("<tr data-id="+data[i].id+" >"+//cria a linha da tabela com os dados
-				"<td>"+data[i].id+"</td>"+
-				"<td>"+data[i].nome+"</td>"+
-				"<td>R$ "+valor+"</td>"+
-				"<td>"+status+"</td>"+//printa o parametro mandado pelo checkStatus
-				"<td>"+data[i].estoque+"</td>"+
-				"<td class='table-option'><button data-toggle='modal' data-target='#dataModal' class='editBtn'><span class='glyphicon glyphicon-pencil'></span></button></td>"+
-				"<td class='table-option'><button data-toggle='modal' data-target='#deleteModal' class='delBtn'><span class='glyphicon glyphicon-trash'></span></button></td></tr>");
-			}
-			else if(statusSelected==""){
-				$("#rowsTable").append("<tr data-id="+data[i].id+" >"+//cria a linha da tabela com os dados
-				"<td>"+data[i].id+"</td>"+
-				"<td>"+data[i].nome+"</td>"+
-				"<td>R$ "+valor+"</td>"+
-				"<td>"+status+"</td>"+//printa o parametro mandado pelo checkStatus
-				"<td>"+data[i].estoque+"</td>"+
-				"<td class='table-option'><button data-toggle='modal' data-target='#dataModal' class='editBtn'><span class='glyphicon glyphicon-pencil'></span></button></td>"+
-				"<td class='table-option'><button data-toggle='modal' data-target='#deleteModal' class='delBtn'><span class='glyphicon glyphicon-trash'></span></button></td></tr>");
-			}
-		}
-	});
-}
-
+//verifica o status do dado do json
 function verifyStatus(statusJson){
 	if (statusJson=="A"){
 		var status="Ativo";
@@ -54,6 +25,7 @@ function verifyStatus(statusJson){
 	}
 }
 
+//checa o status selecionado
 function checkStatusSelect(statusSelected){
 	if (statusSelected=="A"){
 		$("#subtitle").html("Itens Ativos");
@@ -69,56 +41,99 @@ function checkStatusSelect(statusSelected){
 	}
 }
 
-
+//transforma o valor em string
 function valueToString(valor){//transforma o valor de numero para string para colocar na tabela e no modal
 	valor = parseFloat(valor.toString()).toFixed(2).replace(".", ",");
 	return valor;
 }
 
+//cria a tabela através do get
+function createTable(){
+	var statusSelected = checkStatusSelect($("#selectStatus").val());//checa que dado está selecionado
+	cleanTable();//limpa a tabela para atualizar
+	searchClean();//limpa a busca
+	$.get(server, function(data) {//seleciona os dados no json
+		for(var i=0; i<data.length; i++){//percorre todos os dados
+			var valor = valueToString(data[i].valor);//transforma o valor em string
+			var status = verifyStatus(data[i].status);//verifica o status do item
+			if(statusSelected==data[i].status){//se o status selecionado for igual ao status do item
+				$("#rowsTable").append("<tr data-id="+data[i].id+" >"+//cria a linha da tabela com os dados
+				"<td>"+data[i].id+"</td>"+
+				"<td>"+data[i].nome+"</td>"+
+				"<td>R$ "+valor+"</td>"+//printa o valor em string
+				"<td>"+status+"</td>"+//printa o parametro mandado pelo checkStatus
+				"<td>"+data[i].estoque+"</td>"+
+				"<td class='table-option'><button data-toggle='modal' data-target='#dataModal' class='editBtn'><span class='glyphicon glyphicon-pencil'></span></button></td>"+
+				"<td class='table-option'><button data-toggle='modal' data-target='#deleteModal' class='delBtn'><span class='glyphicon glyphicon-trash'></span></button></td></tr>");
+			}
+			else if(statusSelected==""){//se o status selecionado for nem ativo, nem inativo
+				$("#rowsTable").append("<tr data-id="+data[i].id+" >"+//cria a linha da tabela com os dados
+				"<td>"+data[i].id+"</td>"+
+				"<td>"+data[i].nome+"</td>"+
+				"<td>R$ "+valor+"</td>"+//printa o valor em string
+				"<td>"+status+"</td>"+//printa o parametro mandado pelo checkStatus
+				"<td>"+data[i].estoque+"</td>"+
+				"<td class='table-option'><button data-toggle='modal' data-target='#dataModal' class='editBtn'><span class='glyphicon glyphicon-pencil'></span></button></td>"+
+				"<td class='table-option'><button data-toggle='modal' data-target='#deleteModal' class='delBtn'><span class='glyphicon glyphicon-trash'></span></button></td></tr>");
+			}
+		}
+	});
+}
+
+
+//transforma o valor em numero
 function valueToNumber(){//transforma o valor de string para numero, para adicionar no json
 	var valor = $("#valor").val();
 	valor = Number(valor.replace(",", "."));
 	return valor;
 }
 
-function inputsModalAdd(){//limpa as inputs do modal antes de começar a adicionar
-	$("#nome, #valor, #estoque").val("");
-	$("#status").val("A");
+//define as características dos inputs do modal de adicionar
+function inputsModalAdd(){
+	$("#nome, #valor, #estoque").val("");//limpa os inputs
+	$("#status").val("A");//define o status inicial como ativo
 	$("#saveBtn").data('item', "");//define o botão de salvar com o id nulo
-	cleanAlertInput();
+	cleanAlertInput();//limpa o alerta se houver
 }
 
+//define o id do modal e do botão
 function setIdModalEdit(id){
-	$("#dataModal").data('item', id);//define o modal com o id do produto
-	$("#saveBtn").data('item', id);//define o botão save com o id do produto
+	$("#dataModal").data('item', id);
+	$("#saveBtn").data('item', id);
 }
 
-function inputsModalEdit(id){//preenche os inputs do modal editar
+//define as características dos inputs do modal de editar
+function inputsModalEdit(id){
 	$.get(server+id, function(data) {//chama os produtos para preencher as inputs com o produto certo
 		$("#nome").val(data.nome);
 		$("#valor").val(valueToString(data.valor));
 		$("#status").val(data.status);
 		$("#estoque").val(data.estoque);
 	});
-	cleanAlertInput();
+	cleanAlertInput();//limpa o alerta se houver
 }
 
+//define o id do modal de deletar
 function setIdModalDel(id){
-	$("#deleteModal").data('item', id);//define o modal com o id do produto
+	$("#deleteModal").data('item', id);
 }
 
-function deleteJson(id){//manda os dados para realizar a operação de deletar
+//função com os parâmetros de deletar
+function deleteJson(id){
 	ajax("DELETE", id, "", "", "", "", "Item "+id+" excluído com sucesso!");
 }
 
-function addJson(){//manda os dados para realizar a operação de adicionar
+//função com os parâmetros de adicionar
+function addJson(){
 	ajax("POST", "", $("#nome").val(), valueToNumber(), $("#status").val(), $("#estoque").val(), $("#nome").val()+" adicionado com sucesso!");
 }
 
-function editJson(id){//manda os dados para realizar a operação de editar
+//função com os parâmetros de editar
+function editJson(id){
 	ajax("PUT", id, $("#nome").val(), valueToNumber(), $("#status").val(), $("#estoque").val(), "Item "+id+" editado com sucesso!");
 }
 
+//função para realizar as operações
 function ajax(type, id, nome, valor, status, estoque, msg){//realiza a operação desejada, com os dados recebidos
 	$.ajax({
 		type: type,
@@ -134,19 +149,19 @@ function ajax(type, id, nome, valor, status, estoque, msg){//realiza a operaçã
 			createTable();//chama a tabela atualizada
 		},
 		error: function(){
-			alertMsg("Erro na operação!");
+			alertMsg("Erro na operação!");//envia mensagem de erro
 		}
-		
 	});
 }
 
-function alertMsg(mensagem){//cria um alert depois das operações
+//cria um alerta depois das operações 
+function alertMsg(mensagem){
 	$("#bodyMsgAlert").html("<p>"+mensagem+"</p>");
 	$('#msgAlert').modal('show');
 	$('#msgAlert').fadeOut('slow');
 	setTimeout(function(){
-    	$('#msgAlert').modal("hide");
-  	}, 2500);
+		$('#msgAlert').modal("hide");
+	}, 2500);
 }
 
 //seleciona o id na tabela através dos botões de editar e deletar
@@ -160,22 +175,24 @@ function getId(){
 		setIdModalEdit(id);//seta o id da linha no modal de edição
 		inputsModalEdit(id);//preenche os inputs no modal de edição
 	});
-	$("#listaBusca").on('click', '.delSearchBtn', function(){//no clique do botão deletar na tabela, pega o id da linha
+	$("#searchResult").on('click', '.delSearchBtn', function(){//no clique do botão deletar na tabela, pega o id da linha
 		var id = $(this).parents('.well').data('id');
 		setIdModalDel(id);//seta o id da linha no modal de deletar	
 	});
-	$("#listaBusca").on('click', '.editSearchBtn', function(){//no clique do botão editar na tabela, pega o id da linha
+	$("#searchResult").on('click', '.editSearchBtn', function(){//no clique do botão editar na tabela, pega o id da linha
 		var id = $(this).parents('.well').data('id');
 		setIdModalEdit(id);//seta o id da linha no modal de edição
 		inputsModalEdit(id);//preenche os inputs no modal de edição
 	});
 }
 
+//define os títulos dos modais e dos botões de salvar
 function modalTitles(title, button){
 	$("#titleModal").html(title);//cria o título do modal editar/adicionar
 	$("#saveBtn").val(button);//cria o titulo do botão de salvar
 }
 
+//verifica o id do botão de salvar
 function checkSave(saveId){
 	if (saveId == ""){//se o id for vazio, é pra adicionar
 		addJson();
@@ -184,6 +201,7 @@ function checkSave(saveId){
 	}
 }
 
+//cria um alerta para as inputs dentro do modal
 function alertInputs(input){
 	if(input!=""){
 		$(input).css({"border":"1px solid red"});
@@ -195,36 +213,37 @@ function alertInputs(input){
 	}
 }
 
+//limpa o alerta das inputs
 function cleanAlertInput(){
 	$("#nome, #valor, #status, #estoque").css({"border": "1px solid #ccc"});
 	$("#alertInputs").html("");
 }
 
-
-
+//procura o nome do produto
 function nomeSearch(saveId){
 	$.get(server, function(data) {
 		for(var i=0; i<data.length; i++){
 			if($("#nome").val().toLowerCase()==data[i].nome.toLowerCase() && saveId!=data[i].id){
-				alertInputs("");
+				alertInputs("");//se o nome já existe, alerta de item ja cadastado
 				checkName = 1;
 			}
 		}
-		if(checkName==0){
+		if(checkName==0){//se o nome não existe, fecha o modal
 			closeModal(saveId);
 		}
 	});
 }
 
+//fecha o modal
 function closeModal(saveId){
 		$("#dataModal").fadeOut();
 		setTimeout(function(){
-    		$('#dataModal').modal("hide");
-  		});
-  		checkSave(saveId);
+			$('#dataModal').modal("hide");
+		});
+		checkSave(saveId);//verifica o id do botão de salvar
 }
 
-
+//valida o formulário
 function validateForm(saveId){
 	checkName = 0;
 	if($("#nome").val()==""){
@@ -239,125 +258,134 @@ function validateForm(saveId){
 	else if ($("#estoque").val()==""){
 		alertInputs("#estoque");
 	}
-	else{
+	else{//se todos os inputs estão preenchidos, verifica se o nome ja existe
 		nomeSearch(saveId);
 	}
-	$("#nome, #valor, #status, #estoque").focus(function(){
-		$(this).css({"border": "1px solid #00ab9b"});
-		$("#alertInputs").html("");
-	});
-	$("#nome, #valor, #status, #estoque").blur(function(){
-		cleanAlertInput();
-	});
-	
 }
 
-function actions(){//ações dos botões
+//ações dos botões/inputs
+function actions(){
+	//no clique do botão para adicionar produto
 	$("#addBtn").click(function(){
-		modalTitles("Adicionando Produto", "Adicionar Produto");//clica para adicionar o produto, muda o título do modal
+		modalTitles("Adicionando Produto", "Adicionar Produto");//muda o título do modal e do botão salvar
 		inputsModalAdd();//limpa as inputs do modal
 	});
+	//no clique do botão de editar item
 	$("#rowsTable").on('click', '.editBtn', function(){
-		modalTitles("Editando Produto", "Salvar Alterações");//clica para editar o produto, muda o título do modal
+		modalTitles("Editando Produto", "Salvar Alterações");//muda o título do modal e do botão salvar
 	});
-	$("#listaBusca").on('click', '.editSearchBtn', function(){
-		modalTitles("Editando Produto", "Salvar Alterações");//clica para editar o produto, muda o título do modal
+	//no clique do botão de editar no resultado da busca
+	$("#searchResult").on('click', '.editSearchBtn', function(){
+		modalTitles("Editando Produto", "Salvar Alterações");//muda o título do modal e do botão salvar
 	});
-	$('#selectStatus').change(function(){//quando muda a opção do select
+	//no clique do botão de deletar item
+	$("#deleteBtn").click(function(){
+		deleteJson($("#deleteModal").data('item'));//confirma a operação de deletar, pega o id do modal
+	});
+	//no clique do botão de salvar
+	$("#saveBtn").click(function(){
+		validateForm($("#saveBtn").data('item'));//checa se é para adicionar ou editar
+	});
+	//ao selecionar status
+	$('#selectStatus').change(function(){
 		createTable();//checa qual status está selecionado
 	});
-	$("#deleteBtn").click(function(){//clique no botão delete do modal e pega o id do produto
-		deleteJson($("#deleteModal").data('item'));//confirma a operação de deletar
+	//quando der foco nas inputs do modal
+	$("#nome, #valor, #status, #estoque").focus(function(){
+		$(this).css({"border": "1px solid #00ab9b"});//define a cor
+		$("#alertInputs").html("");//limpa o alerta se houver
 	});
-	$("#saveBtn").click(function(){//clica para salvar
-		validateForm($("#saveBtn").data('item'));//check se é para adicionar ou editar
+	//quando sair das inputs do modal
+	$("#nome, #valor, #status, #estoque").blur(function(){
+		cleanAlertInput();//limpa o alerta se houver, definindo a cor inicial
 	});
+	//permite apenas numeros na input estoque
 	$('#estoque').keyup(function () {
-	    if (this.value.match(/[0-9]/)) {
-	        this.value = this.value.replace(/[^0-9]/g, '');
-	    }
+		if (this.value.match(/[0-9]/)) {
+			this.value = this.value.replace(/[^0-9]/g, '');
+		}
+		if ($(this).val().indexOf(0) == 0) {
+			$(this).val($(this).val().replace(0,''));
+		}
 	});
+	//quando sair da input, apaga tudo que não é número
 	$('#estoque').blur(function () {
-	    if (!this.value.match(/[0-9]/)) {
-	        this.value = this.value.replace(/[^0-9]/g, '');
-	    }
+		if (!this.value.match(/[0-9]/)) {
+			this.value = this.value.replace(/[^0-9]/g, '');
+		}
 	});
+	//permite apenas o que está definido na input
 	$("#nome").keyup(function(){
 		var regexp = /[^a-zA-Z- çãõáéíóúàèÌòùâêîôûäëïüöÃÕÁÉÍÓÚÀÈÌÒÙÄÜÏÖËÂÊÎÔÛ]/g;
 		if($(this).val().match(regexp)){
 			$(this).val( $(this).val().replace(regexp,'') );
 		}
 	});
-	$('#nome, #valor, #estoque').bind("paste",function(e) {
-          e.preventDefault();
-     });
+	//não permite colar coisas nas inputs
+	$('#nome, #valor, #estoque').on("paste",function(e) {
+		e.preventDefault();
+	});
+	//não permite enter, ponto ou vírgula na busca
+	$('#searchInput').keypress(function(event){
+		if (event.which == 44 || event.which == 46 || event.which == 13) {
+			event.preventDefault();
+		}
+	});
 }
 
 //mascara para o campo valor do modal
 function maskValor(){
 	$('#valor').priceFormat({
-		prefix: '',
-	    centsSeparator: ',',
-	    thousandsSeparator: ''
+		prefix: '', centsSeparator: ',', thousandsSeparator: ''
 	});
 }
-function busca(){
-	$('#campoBusca').keyup(function(){
-		var searchField = $(this).val();
- 		if(searchField === '')  {
- 			// $('#listaBusca').html('');
- 			$(".listaBusca").fadeOut("fast").css({"display":"none"});
- 			return;
- 		}
- 
-        var regex = new RegExp(searchField, "i");
-        var output = '<div class="row busca" id="rowBusca">';
-        var count = 1;
 
-        $.get(server, function(data) {
-	        $(data).each(function () {
-	        	var nome = this.nome;
-	        	var status;
-	    		 if (nome.search(regex) != -1) {
-	    		 	if(this.status=="A"){
-	    		 		status = "Ativo";
-	    		 	}
-	    		 	else{
-	    		 		status = "Inativo";
-	    		 	}
-	    		 	$(".listaBusca").fadeIn("fast").css({"display":"block"});
-	    		 		// $("#rowBusca").html('<div class="col-md-10 col-md-offset-1 well" data-id='+ this.id+'>'+
-	    		 		// '<div class="col-md-4"><h5>Nome: ' + nome + '</h5>'+
-	    		 		// '<p>Preço: ' + this.valor + '</p>'+
-	    		 		// '</div><div class="col-md-4">'+
-	    		 		// );
-	    		 	output += '<div class="col-md-10 col-md-offset-1 well" data-id='+ this.id+'>';
-		   			output += '<div class="col-md-4">';
-		   			output += '<h5>Nome: ' + nome + '</h5>';
-		   			output += '<p>Preço: ' + this.valor + '</p>'
-		   			output += '</div>';
-		   			output += '<div class="col-md-4">';
-		   			output += '<h5>Status: ' + status + '</h5>';
-		   			output += '<p>Estoque: ' + this.estoque + '</p>'
-		   			output += '</div>';
-		   			output += '<div class="col-md-4">';
-		   			output += '<button data-toggle="modal" data-target="#dataModal" class="editSearchBtn"><span class="glyphicon glyphicon-pencil"></span></button>';
-		   			output += "<button data-toggle='modal' data-target='#deleteModal' class='delSearchBtn'><span class='glyphicon glyphicon-trash'></span></button>";
-		   			output += '</div>';
-		   			output += '</div>';
-		 		}
+//função de busca
+function search(){
+	$('#searchInput').keyup(function(){//quando escrever na input, realiza a busca
+		var searchField = $(this).val();
+		if(searchField === '')  {//se não houver nada no campo de busca, esconde a div de resultados
+			$(".searchResult").fadeOut("fast").css({"display":"none"});
+			return;
+		}
+
+		var regex = new RegExp(searchField, "i");//define a busca sem case sensitive
+		var output = '<div class="row busca" id="rowBusca">';//cria a div para mostras os resultados
+
+		$.get(server, function(data){//procura os dados
+			$(data).each(function (){//percorre um por um
+				var nome = this.nome;
+				var status;
+				if (nome.search(regex) != -1){//se houver dados correspondentes à busca
+					//muda o nome do status
+					if(this.status=="A"){
+						status = "Ativo";
+					}
+					else{
+						status = "Inativo";
+					}
+					//mostra a div de resultados e printa 
+					$(".searchResult").fadeIn("fast").css({"display":"block"});	
+					output += "<div class='col-md-10 col-md-offset-1 well' data-id="+ this.id+">"+
+					"<div class='col-md-2'><h4>" + nome + "</h4></div>"+
+					"<div class='col-md-2'><h4>Preço: " + valueToString(this.valor) + "</h4></div>"+
+					"<div class='col-md-2'><h4>Status: "+ status +"</h4></div>"+
+					"<div class='col-md-4'><h4>Estoque: "+ this.estoque +"</h4></div>"+
+					"<div class='col-md-2 column-btn'><button data-toggle='modal' data-target='#dataModal' class='editSearchBtn btn-busca col-md-6'><span class='glyphicon glyphicon-pencil'></span></button>"+
+					"<button data-toggle='modal' data-target='#deleteModal' class='delSearchBtn btn-busca col-md-6'><span class='glyphicon glyphicon-trash'></span></button></div></div>";
+				}
 			});
 			output += '</div>';
-   			$('#listaBusca').html(output);
+			$('#searchResult').html(output);//define a div com os resultados
 		});
 	});
 }
 
+//na página carregada
 $(document).ready(function(){
 	createTable();//cria a tabela com o $.get
-	busca();
-	actions();//ações de botões 
+	search();//função de busca
+	actions();//ações de botões ou inputs
 	getId();//seleciona o id na tabela através dos botões de editar e deletar
 	maskValor();//mascara para o valor
-	
 });
